8d0b1befe86e228c91aabac7f0fa830020accd3c
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-11-02 20:29:18 +0100

    Fixing component issue in App.jsx, Adding test for search query for user Barack and adding a fixture to mock response. Adding data-cy:s to components.

--- cypress/integration/userCanSeeApplicationTitle.feature.js
+++ cypress/integration/userCanSeeApplicationTitle.feature.js
@@ -1,10 +1,24 @@
-describe('Application main view', () => {
+describe("Application main view", () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.server();
+    cy.route({
+      method: "GET",
+      url:
+        "https://cors-anywhere.herokuapp.com/https://api.github.com/search/users?q=Barack",
+      response: "fixture:search_query.json",
+    });
+    cy.visit("/");
   });
 
-  it('contains titel', () => {
-    cy.get("section[name='title']")
-      .should('contain', 'GitHub Search engine')
+  it("contains titel", () => {
+    cy.get("section[name='title']").should("contain", "GitHub Search engine");
+  });
+
+  context('user searches for user', () => {
+    it('', () => {
+      cy.get("[data-cy='search']").type("Barack");
+      cy.get("[data-cy='search-button']").click()
+      cy.get('#search-results').should('contain', 'PresidentObama')
+    });
   });
 });
